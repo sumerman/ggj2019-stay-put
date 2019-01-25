4a45bc2d1c27c43d45a8e13338a9bb2f2d6091d8
Maria <leitniakris@sumerman-mbpr.student.berlin.sae>
2019-01-26 00:40:18 +0100

    Car script added

---
+++ stay-put/Assets/Scripts/Car.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float speed;
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        target = GameObject.FindGameObjectWithTag("StopPoint");
+        if (target != null)
+        {
+            Vector3 stopPosition = target.GetComponent<Rigidbody>().position;
+            GetComponent<Rigidbody>().position = Vector3.MoveTowards(GetComponent<Rigidbody>().position, stopPosition, speed);
+        }
+    }
+}
