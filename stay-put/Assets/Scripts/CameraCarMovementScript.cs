1ed0572ed1ab26fb3a2868dc8ae1374532712c13
Tobias Kohl <tobiko10@aol.com>
2019-01-26 21:29:56 +0100

    Radio

--- stay-put/Assets/Scripts/CameraCarMovementScript.cs
+++ stay-put/Assets/Scripts/CameraCarMovementScript.cs
@@ -7,13 +7,16 @@ public class CameraCarMovementScript : MonoBehaviour
     [SerializeField]
     private Transform target;
 
-    private AudioSource audioSource;
+    public AudioSource audioSource;
     private Vector3 relativePosition;
+    private float gameTameOnDisable = 0;
+    private float trackTimeOnDisable;
+
     // Start is called before the first frame update
     void Awake()
     {
         relativePosition = gameObject.transform.position - target.gameObject.transform.position;
-        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        //AudioSource audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,12 +34,17 @@ public class CameraCarMovementScript : MonoBehaviour
     public void SwitchTo()
     {
         gameObject.SetActive(true);
-        if (audioSource) audioSource.UnPause();
+        float timeElapsed = Time.fixedTime - gameTameOnDisable;
+        audioSource.time = trackTimeOnDisable + timeElapsed;
+        //if (audioSource) audioSource.mute = false;
+        audioSource.Play();
     }
 
     public void SwitchFrom()
     {
-        if (audioSource) audioSource.Pause();
+        gameTameOnDisable = Time.fixedTime;
+        trackTimeOnDisable = audioSource.time;
+        //if (audioSource) audioSource.mute = true;
         gameObject.SetActive(false);
     }
 }
