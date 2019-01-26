79972f491ba756abb76854c6fce789759dd2acab
Tobias Kohl <tobiko10@aol.com>
2019-01-26 11:04:06 +0100

    Intermediate state of Sat morning

--- stay-put/Assets/Scripts/Car.cs
+++ stay-put/Assets/Scripts/Car.cs
@@ -6,22 +6,81 @@ public class Car : MonoBehaviour
 {
     public float speed;
 
-    public GameObject target;
+    public Waypoint targetWP;
+    public PlayerCharacter pc;
+    //    private Waypoint lastWP;
+
+    private bool stopped;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        pc.car = this;
+        pc.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        target = GameObject.FindGameObjectWithTag("StopPoint");
-        if (target != null)
+        if (targetWP != null && stopped)
         {
-            Vector3 stopPosition = target.GetComponent<Rigidbody>().position;
-            GetComponent<Rigidbody>().position = Vector3.MoveTowards(GetComponent<Rigidbody>().position, stopPosition, speed);
+            if (targetWP.allowDriveOn())
+            {
+                targetWP = targetWP.getNextWaypoint();
+                start();
+            }
+        }
+        if (targetWP != null && !stopped)
+        {
+            moveTowardsNextWaypoint();
+            /*if (targetWP.allowMoveOn())
+            {
+                moveTowardsNextWaypoint();
+            }
+            else
+            {
+                lastWP = targetWP;
+                targetWP = targetWP.getNextWaypoint();
+            }*/
+        }
+        if (stopped && !pc.gameObject.activeSelf && Input.GetKeyDown("space"))
+        {
+            Debug.Log("SPACE");
+            pc.gameObject.SetActive(true);
+            //pc.transform.position = this.transform.position;
+        }
+    }
+
+    private void moveTowardsNextWaypoint()
+    {
+        Vector3 stopPosition = targetWP.transform.position;
+        GetComponent<Rigidbody>().position = Vector3.MoveTowards(GetComponent<Rigidbody>().position, stopPosition, speed);
+    }
+
+    private void stop()
+    {
+        stopped = true;
+    }
+
+    private void start()
+    {
+        stopped = false;
+    }
+
+    public void onPlayerEntered()
+    {
+        
+    }
+
+    public void handleWaypoint(Waypoint wp)
+    {
+        if(wp.allowDriveOn())
+        {
+            targetWP = wp.getNextWaypoint();
+        }
+        else
+        {
+            stop();
         }
     }
 }
--- stay-put/Assets/Scripts/LookAt.cs
+++ stay-put/Assets/Scripts/LookAt.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class LookAt : MonoBehaviour
 {
     public Transform target;
+    private Vector3 relativePosition;
     // Start is called before the first frame update
     void Start()
     {
-
+        relativePosition = gameObject.transform.position - target.gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = target.position + relativePosition;
         transform.LookAt(target);
     }
 }
