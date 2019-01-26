49fea2e1342575593f33c4b7edc5161d9d37b2ac
Tobias Kohl <tobiko10@aol.com>
2019-01-26 21:55:46 +0100

    adding motorSpoundScript

---
+++ stay-put/Assets/Scripts/MotorSoundController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorSoundController : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip startSound;
+    public AudioClip driveSound;
+    public AudioClip stopSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopSound()
+    {
+        source.loop = false;
+        source.clip = stopSound;
+        source.Play();
+    }
+
+    public void StartSound()
+    {
+        source.loop = false;
+        source.clip = startSound;
+        source.Play();
+    }
+
+    public void DriveSound()
+    {
+
+    }
+}
