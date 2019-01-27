30b77e62bf5cff813c993bb68ca72728ed8c1a21
Tobias Kohl <tobiko10@aol.com>
2019-01-27 14:51:42 +0100

    More Scenery

--- stay-put/Assets/Scripts/SmokeStopWaypoint.cs
+++ stay-put/Assets/Scripts/SmokeStopWaypoint.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SmokeStopWaypoint : Stopwaypoint
 {
     public GameObject smoke;
+    public SphereCollider coll;
 
     public override void SpecialCarAction()
     {
         smoke.SetActive(true);
+        coll.radius /= 5;
     }
 }
