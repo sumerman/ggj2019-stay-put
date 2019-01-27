c380ca61ee555e704353af4f7460672b6e2680c1
Valery Meleshkin <valery.meleshkin@gmail.com>
2019-01-27 09:09:48 +0100

    Distance idicator.

---
+++ stay-put/Assets/Scripts/DirectionIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+[RequireComponent(typeof(ParticleSystem))]
+public class DirectionIndicator : MonoBehaviour
+{
+    public Transform target;
+    private ParticleSystem emitter;
+    // Start is called before the first frame update
+    void Start()
+    {
+       emitter =  this.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null) 
+        {
+            emitter.transform.position = Vector3.Lerp(Camera.main.transform.position, target.position, 0.2f);
+            var dist = Vector3.Distance(Camera.main.transform.position, target.position);
+            var shapeMod = emitter.shape;
+            shapeMod.radius =  dist * 0.1f;
+            emitter.emissionRate = dist * 0.9f;
+            emitter.startSize = dist / 6f;
+        }
+    }
+}
